0c828c110f7135957a6a2d008414612452b2e7a7
Lucas Castro <lucasmcastro@gmail.com>
2012-09-29 21:22:02 -0300

    Import Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListaCompras::Application.config.secret_token = '4a349757fbbac814984a599f91ca617e46598ff26cddb01973238785f0282f5347225848782bdb17803b3ff68aee51f757aa9090cb4ced1b6b5094da2c3c6314'
